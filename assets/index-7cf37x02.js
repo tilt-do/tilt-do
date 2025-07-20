0feabf8c75f5ad4e25885df4092048d83a740b1d
tilt-do <info@tilt.do>
2025-07-20 13:21:16 +0700

    Remove form label

--- assets/index-7cf37x02.js
+++ assets/index-7cf37x02.js
@@ -1,4 +1,4 @@
-import{M as rt,T as Oe,R as j,C as I,a as me,O as K,b as ne,E as z,c as uo}from"./index-CeFC0ocp.js";import{v as ho,b as mo}from"./index-CPLvl_7d.js";/**
+import{M as rt,T as Oe,R as j,C as I,a as me,O as K,b as ne,E as z,c as uo}from"./index-CBN_JbiN.js";import{v as ho,b as mo}from"./index-CYsNGXE7.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
