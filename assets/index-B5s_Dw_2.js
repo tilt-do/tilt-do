ec612329dbd26bbe55460818b469042c1fe2094f
tilt-do <info@tilt.do>
2025-07-28 21:20:03 +0700

    Add withdraw confirmation

--- assets/index-B5s_Dw_2.js
+++ assets/index-B5s_Dw_2.js
@@ -1,4 +1,4 @@
-import{M as rt,T as Oe,R as j,C as I,a as me,O as K,b as ne,E as z,c as uo}from"./index-D5q_1Ftp.js";import{v as ho,b as mo}from"./index-DIpCHkC9.js";/**
+import{M as rt,T as Oe,R as j,C as I,a as me,O as K,b as ne,E as z,c as uo}from"./index-zrX6bAWP.js";import{v as ho,b as mo}from"./index-B2JMBcyM.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
