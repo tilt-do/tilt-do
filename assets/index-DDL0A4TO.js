411e3efecd8112e7aa8fdf84eb23a35117a547c6
tilt-do <info@tilt.do>
2025-08-21 12:51:01 +0700

    Fixed description

--- assets/index-DDL0A4TO.js
+++ assets/index-DDL0A4TO.js
@@ -1,4 +1,4 @@
-import{M as rt,T as Oe,R as j,C as I,a as me,O as K,b as ne,E as z,c as uo}from"./index-BLuJY2hR.js";import{v as ho,b as mo}from"./index-CrdOkunh.js";/**
+import{M as rt,T as Oe,R as j,C as I,a as me,O as K,b as ne,E as z,c as uo}from"./index-D3ttNMLw.js";import{v as ho,b as mo}from"./index-ClBXUpuv.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
