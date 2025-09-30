afbd117c1c5fbe26fec67a2c52e59d507d55666c
tilt-do <info@tilt.do>
2025-09-30 20:19:58 +0700

    Add Boost calculator

--- assets/index-CV6OtzQj.js
+++ assets/index-CV6OtzQj.js
@@ -1,4 +1,4 @@
-import{M as rt,T as Oe,R as j,C as I,a as me,O as K,b as ne,E as z,c as uo}from"./index-DUfXTBj-.js";import{v as ho,b as mo}from"./index-BscMXo47.js";/**
+import{M as rt,T as Oe,R as j,C as I,a as me,O as K,b as ne,E as z,c as uo}from"./index-BSCgpz4P.js";import{v as ho,b as mo}from"./index-Cea9Db5m.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
